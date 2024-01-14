c4f97b7fa4287f52e91de6dd44df13fdb5dd6101
MikaPS <mikaps7@gmail.com>
2024-01-13 17:12:24 -0800

    can compute  legal list of line segments demonstrating the path!
    Co-authored-by: Ben Daly <mojius@users.noreply.github.com>

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -12,56 +12,78 @@ def is_point_in_box(point, box):
         return True
     return False
 
-def path_to(source_point, source_box, destination_box, came_from, detail_points):
-    path_points = [source_point]
-    current = destination_box
-    path_boxes = []
-    while current != source_box:
-        destination_point = legal_path_between(source_point, source_box, destination_box, detail_points)
-        path_points.append(destination_point)
-        source_point = destination_point
-        path_boxes.append(current)
-        source_box = current
-        destination_box = came_from[current]
-    path_boxes.append(source_box)
-
-    # While node.prev != null: keep going
-    # prev_node = destination.prev
-    # path = []
-    # while prev_node != None:
-    #     path.append(prev_node)
-    #     prev_node = prev_node.prev
+def path_to(destination_point, source_box, destination_box, came_from, detail_points):
+    # We should only be here if we found a path the canonical FINAL destination box.
+    # The source point we're passing in here is the canonical source point.
+    path_points = [destination_point]
+    current_box = destination_box
+    # path_boxes = []
+    # We are working backwards here, in destination -> source, but also to current box -> came_from[current_box].
+    while current_box != source_box:
+        source_point = legal_path_between(destination_point, came_from[current_box], current_box, detail_points)
+        path_points.append(source_point)
+        destination_point = source_point
+        current_box = came_from[current_box]
     return path_points 
 
-def breadth_first_search(source_point, source_box, destination_box, adjacencies, visited_boxes, detail_points):
+def breadth_first_search(destination_point, source_box, destination_box, adjacencies, visited_boxes, detail_points):
     came_from = dict()
-    queue = [source_box]
-    while queue:
-        current_node = queue.pop()
-        visited_boxes.append(current_node)
-        if current_node == destination_box:
-            return path_to(source_point, source_box, destination_box, came_from, detail_points)
+    q = queue.Queue()
+    q.put(source_box)
+    came_from[source_box] = None
+    while q.qsize() != 0:
+        current_box = q.get()
+        visited_boxes.append(current_box)
+        if current_box == destination_box:
+            return path_to(destination_point, source_box, destination_box, came_from, detail_points)
         else:
-            for new_node in adjacencies[current_node]:
-                if came_from.get(new_node) == None:
-                    came_from[new_node] = current_node
-                    queue.append(new_node)
+            for new_box in adjacencies[current_box]:
+                if came_from.get(new_box) == None:
+                    came_from[new_box] = current_box
+                    q.put(new_box)
 
     return False
 
 def legal_path_between(source_point, source_box, destination_box, detail_points):
-    destination_point = []
+    destination_point = ()
+
+    p_x = source_point[0]
+    p_y = source_point[1]
+
+    x1 = 0
+    x2 = 1
+    
+    y1 = 0
+    y2 = 1
+
+    box_x1 = 0
+    box_x2 = 1
+    box_y1 = 2
+    box_y2 = 3
+
     # x/y range of border is the acceptable range that we can move to from our current point.
-    x_range_of_border = [max(source_box[0], destination_box[0]), min(source_box[1], source_box[1])]
-    y_range_of_border = [max(source_box[2], destination_box[2]), min(source_box[3], source_box[3])]
-    # diff from x/y is the absolute distance between the original point's X/Y and the two x values of the max/min of the box.
-    # We're doing these calculations only for if the x/y point is outside the min/max bounds of the other box's respective trimmed x/y range.
-    diff_from_x = [abs(x_range_of_border[0]-source_point[0]), abs(x_range_of_border[1]-source_point[0])]
-    diff_from_y = [abs(y_range_of_border[0]-source_point[1]), abs(y_range_of_border[1]-source_point[1])]
-    # if the distance between the point and x1 is smaller than the distance from x2, 
-    # the destination point should be x1 (because we want the values that are the closet to the point) 
-    destination_point.append(diff_from_x[0] if diff_from_x[0] < diff_from_x[1] else diff_from_x[1])
-    destination_point.append(diff_from_y[0] if diff_from_y[0] < diff_from_y[1] else diff_from_y[1])
+    x_range_of_border = [max(source_box[box_x1], destination_box[box_x1]), min(source_box[box_x2], source_box[box_x2])]
+    y_range_of_border = [max(source_box[box_y1], destination_box[box_y1]), min(source_box[box_y2], source_box[box_y2])]
+    ## x 
+    # if x < min
+    if (p_x < x_range_of_border[x1]):
+        destination_point += (x_range_of_border[x1],) # min val of range
+    # if x > max
+    elif (p_x > x_range_of_border[x2]):
+        destination_point += (x_range_of_border[x2],) # max val of range
+    # if min < x < max
+    else:
+        destination_point += (p_x,)
+    ## y
+    # if y < min
+    if (p_y < y_range_of_border[y1]):
+        destination_point += (y_range_of_border[y1], ) # min val of range
+    # if y > max
+    elif (p_y > y_range_of_border[y2]):
+        destination_point += (y_range_of_border[y2],) # max val of range
+    # if min < y < max
+    else:
+        destination_point += (p_y, )
     # match the source box with the bounds of the destination box
     detail_points[source_box] = destination_point
     return destination_point
@@ -70,8 +92,9 @@ def find_path (source_point, destination_point, mesh):
     # Mesh: has a list of boxes and a list of adjacencies
 
     # We are searching through the entire dictionary of boxes to find the boxes with the src/desination points in them.
-    
-    src_dst_boxes = []
+    source_box = None
+    destination_box = None
+    # src_dst_boxes = []
     visited_boxes = []
     detail_points = {}
     
@@ -79,27 +102,23 @@ def find_path (source_point, destination_point, mesh):
         if (source_point == destination_point):
             if is_point_in_box(source_point, current_box):
                 return ([source_point, destination_point], current_box)
-        if is_point_in_box(source_point, current_box) or is_point_in_box(destination_point, current_box):
-            print("box of current point: ", current_box)
-            src_dst_boxes.append(current_box)
-    # print("mesh list:", mesh["adj"])
-    print("visited boxes: ", src_dst_boxes, " length: ", len(src_dst_boxes), " type: ", type(src_dst_boxes))
+        if is_point_in_box(source_point, current_box):
+            source_box = current_box
+        elif  is_point_in_box(destination_point, current_box):
+            destination_box = current_box
+            
 
-    if (len(src_dst_boxes) == 2):
-        print("source box: ", src_dst_boxes[0], " dest box: ", src_dst_boxes[1])
-        # print(legal_path_between(source_point, src_dst_boxes[0], src_dst_boxes[1]))
-
-        result = breadth_first_search(source_point, src_dst_boxes[0], src_dst_boxes[1], mesh["adj"], visited_boxes, detail_points)
+    if (source_box != None and destination_box != None):
+        result = breadth_first_search(destination_point, source_box, destination_box, mesh["adj"], visited_boxes, detail_points)
         if (result == False):
-            print("No path!")
-            return
+            return [], visited_boxes
         else:
+           result.append(source_point)
+           result.reverse()
            return result, visited_boxes
             
     else: 
-        print("No path!")
-        return
-    # print(visited_boxes)
+        return [], visited_boxes
 
     """
     Searches for a path from source_point to destination_point through the mesh
